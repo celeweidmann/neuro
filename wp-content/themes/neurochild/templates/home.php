96025dfb159a488c677dc6bfd80da6509c998de0
Ale Prieto <aleprieto@gmail.com>
2018-06-12 21:01:51 -0300

    Se programa el curso destacado del hero. Se programa la sección institucional para manejarla con ACF.

--- wp-content/themes/neurochild/templates/home.php
+++ wp-content/themes/neurochild/templates/home.php
@@ -12,61 +12,70 @@
  * @package storefront
  */
 
+$cursosDestacados = get_posts(array(
+  'posts_per_page'	=> 1,
+  'post_type'			=> 'course',
+  'orderby' => 'date',
+  'order' => 'DESC',
+  'meta_query' => array(
+    array(
+        'key' => '_course_featured',
+        'value' => 'featured',
+        'compare' => '=',
+    )
+  )
+));
+
+$cursos = get_posts(array(
+  'posts_per_page'	=> 3,
+  'post_type'			=> 'course',
+  'orderby' => 'date',
+  'order' => 'DESC'
+));
+
 get_header(); ?>
 
+<?php # Curso Destacado; ?>
+<?php $cursoDestacado = (sizeof($cursosDestacados) >= 1) ? $cursosDestacados[0] : $cursos[0]; ?>
 <section class="row home__featured" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/dist/images/bg-home-block1.png'; ?>);">
   <div class="col-md-6 text-right">
-    <h1>Diplomado Internacional en Liderazgo Neuroespiritual</h1>
+    <h1><?php echo $cursoDestacado->post_title; ?></h1>
   </div>
   <div class="col-md-6">
-    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique libero augue, ut commodo lacus sodales at. Pellentesque scelerisque metus at nisi pellentesque euismod. Donec nisl ante, interdum id libero at, tempus tincidunt lorem. Cras dapibus ante vel elit facilisis, a semper ligula viverra. Donec non libero vel ex luctus volutpat gravida pretium urna. Ut laoreet est id turpis euismod, ut euismod augue luctus.</div>
-    <div><button class="btn">Ver curso</button></div>
+    <div><?php echo wp_trim_words(get_the_excerpt($cursoDestacado->ID), 20); ?></div>
+    <div><a href="<?php echo get_the_permalink($cursoDestacado->ID); ?>" class="btn">Ver curso</a></div>
   </div>
 </section>
 
+<?php # Institucional; ?>
 <section class="row home__institucional">
   <div class="col-md-6 text-right">
-    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique libero augue, ut commodo lacus sodales at. Pellentesque scelerisque metus at nisi pellentesque euismod. Donec nisl ante, interdum id libero at, tempus tincidunt lorem. Cras dapibus ante vel elit facilisis, a semper ligula viverra. Donec non libero vel ex luctus volutpat gravida pretium urna. Ut laoreet est id turpis euismod, ut euismod augue luctus.</div>
-    <div><button class="btn">Conocenos</button></div>
+    <div><?php the_field('inicio_institucional_text'); ?></div>
+    <div><a href="<?php echo esc_url( get_field('inicio_institucional_link') ); ?>" class="btn">Conocenos</a></div>
   </div>
   <div class="col-md-6">
-    <!-- <img class="mx-auto" src="http://via.placeholder.com/170x150"> -->
     <img class="mx-auto" src="<?php echo get_stylesheet_directory_uri() . '/dist/images/logo-neuroespiritual.png'; ?>" alt="logo">
   </div>
 </section>
 
-<section class="row home__courses">
-  <div class="col-sm-4">
-    <div class="card">
-      <img class="card-img-top" src="http://via.placeholder.com/430x380" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Diplomado internacional...</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <div class="text-center"><a href="#" class="btn btn-primary">Ver curso</a></div>
+<?php # Lista de Cursos; ?>
+<?php if( $cursos ): ?>
+  <section class="row home__courses">
+    <?php foreach( $cursos as $curso ):?>
+      <div class="col-sm-6 col-md-4">
+        <div class="card">
+          <img class="card-img-top" src="<?php echo get_the_post_thumbnail_url($curso->ID, 'thumbnail'); ?>" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title"><?php echo get_the_title($curso->ID); ?></h5>
+            <p class="card-text"><?php echo wp_trim_words(get_the_excerpt($curso->ID), 20); ?></p>
+            <div class="text-center"><a href="<?php echo get_the_permalink($curso->ID); ?>" class="btn btn-primary">Ver curso</a></div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  <div class="col-sm-4">
-    <div class="card">
-      <img class="card-img-top" src="http://via.placeholder.com/430x380" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">Couch de Liderazgo...</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <div class="text-center"><a href="#" class="btn btn-primary">Ver curso</a></div>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-4">
-    <div class="card">
-      <img class="card-img-top" src="http://via.placeholder.com/430x380" alt="Card image cap">
-      <div class="card-body">
-        <h5 cl  ass="card-title">Curso de liderazgo...</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <div class="text-center"><a href="#" class="btn btn-primary">Ver curso</a></div>
-      </div>
-    </div>
-  </div>
-</section>
+    <?php endforeach; ?>
+  </section>
+  <?php wp_reset_postdata(); ?>
+<?php endif; ?>
 
 <?php
 get_footer();
