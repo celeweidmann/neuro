37d3bed608e90dd4322d76a072a9368cf26cfd8f
Ale Prieto <aleprieto@gmail.com>
2018-06-13 13:55:52 -0300

    Se agregan estilos para el login y se modifican algunas variables.

--- wp-content/mu-plugins/colibri-custom-config.php
+++ wp-content/mu-plugins/colibri-custom-config.php
@@ -23,3 +23,27 @@ function colibre_change_sensei_label() {
   }
 }
 add_action( 'admin_menu', 'colibre_change_sensei_label', 11 );
+
+/**
+ * Cambiar la url en la pantalla de login
+ */
+function colibre_header_url() {
+  return esc_url(site_url( '/'));
+}
+add_filter('login_headerurl', 'colibre_header_url');
+
+/**
+ * Cargar css en la pantalla de login
+ */
+function colibre_login_css() {
+  wp_enqueue_style( 'colibre_main_styles', get_stylesheet_directory_uri() . '/style_login.css');
+}
+add_action('login_enqueue_scripts', 'colibre_login_css');
+
+/**
+ * Cambiar titulo en la pantalla de login
+ */
+function colibre_login_title() {
+  return get_bloginfo('name');
+}
+add_filter('login_headertitle', 'colibre_login_title');
