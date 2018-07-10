2d45caa244983f05a333ee9a97e47741a5c2d6b9
Ale Prieto <aleprieto@gmail.com>
2018-07-10 18:46:58 -0300

    Se reestructura el footer y se extrae el curriculum para que solo se despliegue en la home.

---
+++ wp-content/themes/neurochild/partials/content-curriculum.php
@@ -0,0 +1,46 @@
+<section class="curriculum" role="contentinfo">
+  <div class="curriculum__cv-block">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="cv-card">
+            <div class="cv-card__photo">
+              <img class="mx-auto d-block rounded-circle" src="<?php echo get_stylesheet_directory_uri() . '/dist/images/foto-jose.png'; ?>" alt="Foto Jose Bizai">
+            </div>
+            <div class="cv-card__logo">
+              <img class="mx-auto d-block" src="<?php echo get_stylesheet_directory_uri() . '/dist/images/logo-jose-bizai.png'; ?>" alt="Logo Jose Bizai">
+            </div>
+            <div class="cv-card__text text-center">
+              <span>Director del Diplomado y docente responsable.</span>
+            </div>
+            <div class="cv-card__buttons text-center">
+              <a class="btn" target="_blank" href="<?php the_field('contact_curriculum_vitae', 'option'); ?>">cv</a>
+              <a class="btn" target="_blank" href="<?php the_field('contact_site', 'option'); ?>">web</a>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-md-8">
+          <div class="row curriculum__certificado">
+          <div class="col-md-2">
+              <img class="mx-auto" src="<?php echo get_stylesheet_directory_uri() . '/dist/images/logo-certificado.png'; ?>">
+            </div>
+            <div class="col-md-10">
+              <h3>Certificado Internacional</h3>
+              <p>Nuestro Diplomado cuenta con la <b>Certificación de Competencia Educacional Profesional en Coaching C.C.E.P.C.</b> de AEAPro con Registro Número AR619.1/18P, bajo normas ISO 9001 y con la certificación de Quality System IQNet.</p>
+            </div>
+          </div>
+          <hr>
+          <div class="row">
+            <div class="col-md-6">
+              <img class="mx-auto" src="<?php echo get_stylesheet_directory_uri() . '/dist/images/logo-aeapro.png'; ?>">
+            </div>
+            <div class="col-md-6">
+              <img class="mx-auto" src="<?php echo get_stylesheet_directory_uri() . '/dist/images/logo-red-latino.png'; ?>">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
