dda766e6bc0c8bcb1018c5e208cbd09f2b7d6339
Ale Prieto <aleprieto@gmail.com>
2018-07-17 16:34:12 -0300

    - Se agrega un margen auto al card-deck para centrarlo. - Se eliminan comentarios y código innecesarios.

--- wp-content/themes/neurochild/partials/content-cursos.php
+++ wp-content/themes/neurochild/partials/content-cursos.php
@@ -5,40 +5,21 @@ $cursos = get_posts(array(
   'orderby' => 'date',
   'order' => 'DESC'
 ));
-// $cursos[] = $cursos[0];
-$colClases = '';
-$colOffset = '';
-switch ( sizeof($cursos) ) {
-  case 1:
-    $colClases = 'col-sm-12 col-md-12';
-    break;
-  case 2:
-    $colClases = 'col-sm-6 col-md-4';
-    $colOffset = ' offset-md-2';
-    break;
-  default:
-    $colClases = 'col-sm-6 col-md-4';
-    break;
-}
 ?>
 <?php if( $cursos ): ?>
   <section class="row home-scroll home__courses">
-    <div class="card-deck">
-      <?php $i = 0; ?>
+    <div class="card-deck home__courses__card-deck">
       <?php foreach( $cursos as $curso ):?>
-        <!-- <div class="<?php echo $colClases; ?><?php echo $i == 0 ? $colOffset : ''; ?>"> -->
-          <div class="card home__courses__course">
-            <img class="card-img-top home__courses__course__image" src="<?php echo get_the_post_thumbnail_url($curso->ID, 'thumbnail'); ?>" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title"><?php echo get_the_title($curso->ID); ?></h5>
-              <p class="card-text"><?php echo wp_trim_words(get_the_excerpt($curso->ID), 10); ?></p>
-            </div>
-            <div class="card-footer">
-              <div class="text-center"><a href="<?php echo get_the_permalink($curso->ID); ?>" class="btn btn-primary">Ver curso</a></div>
-            </div>
+        <div class="card home__courses__course">
+          <img class="card-img-top home__courses__course__image" src="<?php echo get_the_post_thumbnail_url($curso->ID, 'thumbnail'); ?>" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title"><?php echo get_the_title($curso->ID); ?></h5>
+            <p class="card-text"><?php echo wp_trim_words(get_the_excerpt($curso->ID), 10); ?></p>
           </div>
-        <!-- </div> -->
-        <?php $i++; ?>
+          <div class="card-footer">
+            <div class="text-center"><a href="<?php echo get_the_permalink($curso->ID); ?>" class="btn btn-primary">Ver curso</a></div>
+          </div>
+        </div>
       <?php endforeach; ?>
     </div>
   </section>
