c921e3266bda479c1acbcd682d5f61791c880432
celes <celeweidmann@gmail.com>
2018-08-21 17:18:47 -0300

    favicon

--- wp-content/themes/neurochild/header.php
+++ wp-content/themes/neurochild/header.php
@@ -15,6 +15,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/favicon.png" />
+
 <?php wp_head(); ?>
 </head>
 
